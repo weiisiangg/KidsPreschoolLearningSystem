90d311cd06caca9122aacff072a76e68b332092b
weiisiangg <118103672+weiisiangg@users.noreply.github.com>
2023-06-23 16:53:42 +0800

    add style on table

--- manageStudent.php
+++ manageStudent.php
@@ -17,29 +17,6 @@ function connect(){
 
 $link = connect();
 
-if (isset($_POST["insert"])) {
-    $loginid = isset($_POST['loginID']) ? $_POST['loginID'] : '';
-    $pwd = isset($_POST['pwd']) ? $_POST['pwd'] : '';
-    $name = isset($_POST['name']) ? $_POST['name'] : '';
-    $date = isset($_POST['date']) ? $_POST['date'] : '';
-    $edustage = isset($_POST['edustage']) ? $_POST['edustage'] : '';
-    $pname = isset($_POST['pname']) ? $_POST['pname'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $contact = isset($_POST['contact']) ? $_POST['contact'] : '';
-    $address = isset($_POST['address']) ? $_POST['address'] : '';
-    $sdate = isset($_POST['sdate']) ? $_POST['sdate'] : '';
-    $role = isset($_POST['role']) ? $_POST['role'] : '';
-
-    $query = "INSERT INTO user (loginID, password, name, dob, education_stage, parentName, email, contact, address, startDate, role) VALUES ('$loginid', '$pwd', '$name', '$date', '$edustage', '$pname', '$email', '$contact', '$address', '$sdate', '$role')";
-
-
-    if(mysqli_query($link, $query)) {
-        echo "Record inserted successfully.";
-    } else {
-        echo "Error: " . mysqli_error($link);
-    }
-}
-
 if (isset($_POST["update"])) {
     $loginid = isset($_POST['loginID']) ? $_POST['loginID'] : '';
     $pwd = isset($_POST['pwd']) ? $_POST['pwd'] : '';
@@ -115,7 +92,7 @@ if (isset($_POST["update"])) {
                          <li><a href="teacher.php" class="smoothScroll">Home</a></li>
                          <li><a href="viewprofile.php?profileid=<?php echo $user_id?>" class="smoothScroll">Profile</a></li>
                          <li><a href="manageStudent.php?profileid=<?php echo $user_id?>" class="smoothScroll">Manage Student</a></li>
-                         <li><a href="manageCourse.php?profileid=<?php echo $user_id?>" class="smoothScroll">Manage Course</a></li>
+                         <li><a href="manageCourse.php" class="smoothScroll">Manage Course</a></li>
                     </ul>
 
                     <ul class="nav navbar-nav navbar-right">   
@@ -131,7 +108,7 @@ if (isset($_POST["update"])) {
                                    <li role="separator" class="divider"></li>
                                    <li><a href ="viewprofile.php?profileid=<?php echo $user_id?>">User Profile</a></li>
                                    <li><a href="manageStudent.php?profileid=<?php echo $user_id?>">Manage Student</a></li>
-                                   <li><a href="manageCourse.php?profileid=<?php echo $user_id?>">Manage Course</a></li>
+                                   <li><a href="manageCourse.php">Manage Course</a></li>
                                    <li><a href="logout.php">Log Out</a></li>
                               </ul>
                     </ul>
@@ -139,127 +116,82 @@ if (isset($_POST["update"])) {
 
           </div>
      </section>
-     <section>
-     <h1 style="color:black; text-align:top; margin-top: -10px;">Student Lists</h1>
-<div id="studentListContainer">   
-<div  class="col-lg-12">
-<!-- <table class="table table-bordered"> -->
-     <table>
-    <thead>
-      <tr><br><br>
-        <th>Login ID</th>
-        <th>Password</th>
-        <th>Name</th>
-        <th>Dob</th>
-        <th>Education Stage</th>
-        <th>Parent Name</th>
-        <th>Email</th>
-        <th>Contact</th>
-        <th>Address</th>
-        <th>Start Date</th>
-        <th>Role</th>
-      </tr>
-    </thead>
-    <tbody>
-        <?php
-        $res=mysqli_query($link,"select * from user WHERE role = 1");
-        while($row=mysqli_fetch_array($res))
-        {
-            echo "<tr>";
-            echo "<td>"; echo $row["loginID"]; echo"</td>";
-            echo "<td>"; echo $row["password"]; echo"</td>";
-            echo "<td>"; echo $row["name"]; echo"</td>";
-            echo "<td>"; echo $row["dob"]; echo"</td>";
-            echo "<td>"; echo $row["education_stage"]; echo"</td>";
-            echo "<td>"; echo $row["parentName"]; echo"</td>";
-            echo "<td>"; echo $row["email"]; echo"</td>";
-            echo "<td>"; echo $row["contact"]; echo"</td>";
-            echo "<td>"; echo $row["address"]; echo"</td>";
-            echo "<td>"; echo $row["startDate"]; echo"</td>";
-            echo "<td>"; echo $row["role"]; echo"</td>";
-            echo "<td>";
-            $id = htmlspecialchars($row["loginID"] ?? ""); // Sanitize the value and provide a default empty string if it's not set
-            echo '<a href="edit.php?id=' . $row["loginID"] . '"><button type="button" class="btn btn-success">Edit</button></a>';
-            echo '<form action="manageStudent.php?status=2" method="post">';
-            echo '<input type="text" id="loginID" name="loginID" value=' . $row["loginID"] . ' style="display:none;">';
-            echo '<input type="text" id="password" name="password" value=' . $row["password"] . ' style="display:none;">';
-            echo '<input type="text" id="name" name="name" value=' . $row["name"] . ' style="display:none;">';
-            echo '<input type="text" id="dob" name="dob" value=' . $row["dob"] . ' style="display:none;">';
-            echo '<input type="text" id="education_stage" name="education_stage" value=' . $row["education_stage"] . ' style="display:none;">';
-            echo '<input type="text" id="parentName" name="parentName" value=' . $row["parentName"] . ' style="display:none;">';
-            echo '<input type="text" id="email" name="email" value=' . $row["email"] . ' style="display:none;">';
-            echo '<input type="text" id="contact" name="contact" value=' . $row["contact"] . ' style="display:none;">';
-            echo '<input type="text" id="address" name="address" value=' . $row["address"] . ' style="display:none;">';
-            echo '<input type="text" id="startDate" name="startDate" value=' . $row["startDate"] . ' style="display:none;">';
-            echo '<input type="text" id="role" name="role" value=' . $row["role"] . ' style="display:none;">';
-            echo '<input type="text" id="status" name="status" value="2" style="display:none;">';
-            echo '<input type="text" id="update" name="update" value="tuna" style="display:none;">';
-            echo '<button type="submit">Delete</button>';
-            echo '</form>';
-            echo "</td>";
-            echo "</tr>";
-        }
-        ?>
-</tbody>
-  </table>
-</div>
+
+     <section id = "manageStudent">
+     <div class="container">
+          <div id="courseList" class="section">
+               <div class="section-center">
+                    <div class="row">
+                    <h1 style="color:black; text-align:top; margin-top: -10px;">Student Lists</h1>
+                    <table class= "table table-boardered table-hover"> 
+
+                         <thead>
+                         <tr><br><br>
+                              <th>No.</th>
+                              <th>Login ID</th>
+                              <th>Password</th>
+                              <th>Name</th>
+                              <th>Dob</th>
+                              <th>Education Stage</th>
+                              <th>Parent Name</th>
+                              <th>Email</th>
+                              <th>Contact</th>
+                              <th>Address</th>
+                              <th>Start Date</th>
+                         </tr>
+                         </thead>
+
+                         <tbody>
+                              <?php
+                                   $i = 1;
+                                   $res=mysqli_query($link,"select * from user WHERE role = 1");
+                                   while($row=mysqli_fetch_array($res))
+                                   {
+                                        echo "<tr>";
+                                        echo "<td>"; echo $i; echo"</td>";
+                                        echo "<td>"; echo $row["loginID"]; echo"</td>";
+                                        echo "<td>"; echo $row["password"]; echo"</td>";
+                                        echo "<td>"; echo $row["name"]; echo"</td>";
+                                        echo "<td>"; echo $row["dob"]; echo"</td>";
+                                        echo "<td>"; echo $row["education_stage"]; echo"</td>";
+                                        echo "<td>"; echo $row["parentName"]; echo"</td>";
+                                        echo "<td>"; echo $row["email"]; echo"</td>";
+                                        echo "<td>"; echo $row["contact"]; echo"</td>";
+                                        echo "<td>"; echo $row["address"]; echo"</td>";
+                                        echo "<td>"; echo $row["startDate"]; echo"</td>";
+                                        echo "<td>";
+                                        $id = htmlspecialchars($row["loginID"] ?? ""); // Sanitize the value and provide a default empty string if it's not set
+                                        echo '<a href="edit.php?id=' . $row["loginID"] . '"><button type="button" class="btn btn-success">Edit</button></a>';
+                                        echo '<form action="manageStudent.php?status=2" method="post">';
+                                        echo '<input type="text" id="loginID" name="loginID" value=' . $row["loginID"] . ' style="display:none;">';
+                                        echo '<input type="text" id="password" name="password" value=' . $row["password"] . ' style="display:none;">';
+                                        echo '<input type="text" id="name" name="name" value=' . $row["name"] . ' style="display:none;">';
+                                        echo '<input type="text" id="dob" name="dob" value=' . $row["dob"] . ' style="display:none;">';
+                                        echo '<input type="text" id="education_stage" name="education_stage" value=' . $row["education_stage"] . ' style="display:none;">';
+                                        echo '<input type="text" id="parentName" name="parentName" value=' . $row["parentName"] . ' style="display:none;">';
+                                        echo '<input type="text" id="email" name="email" value=' . $row["email"] . ' style="display:none;">';
+                                        echo '<input type="text" id="contact" name="contact" value=' . $row["contact"] . ' style="display:none;">';
+                                        echo '<input type="text" id="address" name="address" value=' . $row["address"] . ' style="display:none;">';
+                                        echo '<input type="text" id="startDate" name="startDate" value=' . $row["startDate"] . ' style="display:none;">';
+                                        echo '<input type="text" hidden id="role" name="role" value=' . $row["role"] . ' style="display:none; ">';
+                                        echo '<input type="text" hidden id="status" name="status" value="2" style="display:none; ">';
+                                        echo '<input type="text" id="update" name="update" value="tuna" style="display:none; ">';
+                                        echo '<button type="submit"  class="btn btn-danger">Delete</button>';
+                                        echo '</form>';
+                                        echo "</td>";
+                                        echo "</tr>";
+                                   }
+                              ?>
+                         </tbody>
+                    </table>
+                    </div>
+                    <input type="button" class="button-active" style="width:250px; margin-left:35%; margin-bottom:3%; border-radius:20px; border: 1px solid transparent;
+                    background: palevioletred; color: #ffffff; text-transform: capitalize; cursor: pointer; font-size: 20px;" onMouseOver="this.style.background='rgba(253,187,45,1)'" 
+                    onMouseOut="this.style.background='palevioletred'" onclick="location.href='newStudent.php'" value="Create a new student">     
+               </div>
+          </div>
      </div>
-    </section>
-    
-<!-- <div class="container"> -->
-        <div>
-  <h2>Preschool System</h2>
-  <form action="" method="post">
-    <div class="form-group">
-      <label for="loginID">Login ID:</label>
-      <input type="text" class="form-control" id="loginID" placeholder="Enter Login ID" name="loginID" required>
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd" required>
-    </div>
-    <div class="form-group">
-      <label for="name">Name:</label>
-      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
-    </div>
-    <div class="form-group">
-      <label for="date">Date of Birth:</label>
-      <input type="date" class="form-control" id="date" name="date" required>
-    </div>
-    <div class="form-group">
-      <label for="edustage">Education Stage:</label>
-      <select class="form-control" id="edustage" name="edustage">
-        <option value="Pre-K">Pre-K</option>
-        <option value="Kindergarten">Kindergarten</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="pname">Parent Name:</label>
-      <input type="text" class="form-control" id="pname" placeholder="Enter parent name" name="pname" required>
-    </div>
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
-    </div>
-    <div class="form-group">
-      <label for="contact">Contact:</label>
-      <input type="tel" name="contact">
-    </div>
-    <div class="form-group">
-      <label for="address">Address:</label>
-      <input type="text" class="form-control" id="address" placeholder="Enter address" name="address" required>
-    </div>
-    <div class="form-group">
-      <label for="sdate">Start Date:</label>
-      <input type="date" class="form-control" id="sdate" name="sdate" required>
-    </div>
-       <div class="form-group">
-        <label for="role">Role:</label>
-        <input type="text" class="form-control" id="role" name="role" value="1" readonly>
-    </div>
-    <button type="submit" class="btn btn-primary" name="insert">Add Student</button><br><br>
-  </form>
-</div>
+    </section>     
 
      <!-- FOOTER -->
      <footer id="footer">
